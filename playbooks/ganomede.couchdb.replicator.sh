bb4d6424804b251dbbbe15c831b78ce291f42615
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-02-19 09:31:40 +0200

    Rely more on mesos-dns

--- playbooks/ganomede.couchdb.replicator.sh
+++ playbooks/ganomede.couchdb.replicator.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: replicate using mesos-dns (so data doesn't have to go throught the proxy)
-
 {% if index == "0" %}
 OTHER_INDEX="1"
 {% else %}
@@ -9,41 +7,31 @@ OTHER_INDEX="0"
 {% endif %}
 
 DATABASES="
-    {% for database in couchdb_databases %}
-      {{ database }}
-    {% endfor %}
+{% for database in couchdb_databases %}
+  {{ database }}
+{% endfor %}
 "
-INTERNAL_LB="http://{{ hostvars[groups['marathon-lb-internal'][0]].tun0 }}:9091"
-FROM_URL="$INTERNAL_LB"
-
 for DB_NAME in $DATABASES; do
-  IP=${FROM_URL%:*}
   echo "New replication:"
 cat << EOF > tmp-replicator.json
 {
-"_id": "replicate-$DB_NAME",
+"_id": "replicate-$DB_NAME-$OTHER_INDEX-to-{{index}}",
 "create_target": true,
 "continuous": true,
-"source": {
-"url": "$FROM_URL/$DB_NAME",
-"headers": {"X-Marathon-App-Id":"/{{ app_name }}-$OTHER_INDEX"}
-},
-"target": {
-"url": "$FROM_URL/$DB_NAME",
-"headers": {"X-Marathon-App-Id":"/{{ app_name }}-{{ index }}"}
-}
+"source": "http://couchdb-$OTHER_INDEX.{{dns_domain}}:{{config.couchdb.port}}/$DB_NAME",
+"target": "http://couchdb-{{index}}.{{dns_domain}}:{{config.couchdb.port}}/$DB_NAME"
 }
 EOF
 
   cat tmp-replicator.json
-  sleep 1
-  echo 3
-  sleep 1
-  echo 2
-  sleep 1
-  echo 1
-  sleep 1
-  echo
-  curl -H "X-Marathon-App-Id: {{ marathon_app_id}}" -H "Content-type: application/json" $INTERNAL_LB/_replicator -d @tmp-replicator.json
+  #sleep 1
+  #echo 3
+  #sleep 1
+  #echo 2
+  #sleep 1
+  #echo 1
+  #sleep 1
+  #echo
+  curl -H "Content-type: application/json" http://couchdb-{{index}}.{{dns_domain}}:{{config.couchdb.port}}/_replicator -d @tmp-replicator.json
   echo
 done
--- scripts/broadcast-all-docker-images.sh
+++ scripts/broadcast-all-docker-images.sh
@@ -5,12 +5,14 @@ if [ -z $INVENTORY ]; then
     echo "usage: $0 <inventory-file>"
     exit 1
 fi
-fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" redis:alpine
-fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" python:3-alpine
-fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/couchdb:1.6.1
-fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/haproxy-redis-lb:direct
-fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/marathon-redis-sentinel:direct
+
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" ganomede/avatars:v1.0.7
 fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" ganomede/data:v0.1.2
 fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" ganomede/directory:v0.0.4
 fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" ganomede/events:v1.0.1
-fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" ganomede/avatars:v1.0.7
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/couchdb-haproxy:simple
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/couchdb:1.6.1
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/haproxy-redis-lb:direct
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" jeko/marathon-redis-sentinel:direct
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" python:3-alpine
+fovea-marathon-base/scripts/broadcast-docker-image.sh "$INVENTORY" redis:alpine
